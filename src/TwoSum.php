166e8ec5bd2288514b37f68ea5da59e4c795bf5b
Vladislav Shcheglov <vladislav@scheglov.org>
2023-11-26 12:49:28 +0800

    Add space

--- src/TwoSum.php
+++ src/TwoSum.php
@@ -7,7 +7,7 @@ class TwoSum
     function twoSum($nums, $target)
     {
         $map = [];
-        foreach($nums as $index => $num) {
+        foreach ($nums as $index => $num) {
             if (isset($map[$target - $num])) {
                 return [$map[$target - $num], $index];
             }
