2bb6f0856b49d3850db9c33471a967bdf7a2666c
Vladislav Shcheglov <vladislav@scheglov.org>
2023-11-29 15:14:07 +0800

    More strict test for remove duplicates

--- 26_remove_duplicates_from_sorted_array/26_remove_duplicates_from_sorted_array_test.go
+++ 26_remove_duplicates_from_sorted_array/26_remove_duplicates_from_sorted_array_test.go
@@ -2,19 +2,25 @@ package _26_remove_duplicates_from_sorted_array
 
 import "testing"
 
-func TestSearch(t *testing.T) {
+func TestRemoveDuplicates(t *testing.T) {
 	testCases := []struct {
-		nums     []int
-		expected int
+		nums         []int
+		expected     int
+		expectedNums []int
 	}{
-		{[]int{1, 1, 2}, 2},
-		{[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, 5},
+		{[]int{1, 1, 2}, 2, []int{1, 2}},
+		{[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, 5, []int{0, 1, 2, 3, 4}},
 	}
 
 	for _, tc := range testCases {
-		result := RemoveDuplicates(tc.nums)
-		if result != tc.expected {
-			t.Errorf("RemoveDuplicates(%v) = %d; expected %d", tc.nums, result, tc.expected)
+		k := RemoveDuplicates(tc.nums)
+		if k != tc.expected {
+			t.Errorf("RemoveDuplicates(%v) = %d; expected %d", tc.nums, k, tc.expected)
+		}
+		for i := 0; i < k; i++ {
+			if tc.nums[i] != tc.expectedNums[i] {
+				t.Errorf("After RemoveDuplicates, nums[%d] = %d; expected %d", i, tc.nums[i], tc.expectedNums[i])
+			}
 		}
 	}
 }
